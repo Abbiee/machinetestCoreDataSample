ddd84682b14c2ca27e1287e0b6bebef776764c48
Abbie <abhijithviswan3@gmail.com>
2018-01-08 00:43:08 +0530

    Add files via upload

---
+++ basicCoreDataExample-master/CoreDataExample/DisplayTableViewController.swift
@@ -0,0 +1,112 @@
+//
+//  DisplayTableViewController.swift
+//  CoreDataExample
+//
+//  Created by Farhan Syed on 4/16/17.
+//  Copyright © 2017 Farhan Syed. All rights reserved.
+//
+
+import UIKit
+
+class DisplayTableViewController: UITableViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var items: [Item] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "My Entries"
+        
+        self.tableView.estimatedRowHeight = 44
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+ 
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
+    }
+    
+    
+    func fetchData() {
+        
+        do {
+            items = try context.fetch(Item.fetchRequest())
+            print(items)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Couldn't Fetch Data")
+        }
+        
+    }
+
+}
+
+
+extension DisplayTableViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        let date = items.reversed()[indexPath.row].date
+        let time = items.reversed()[indexPath.row].time
+        
+        cell.textLabel?.text = items.reversed()[indexPath.row].name
+        
+        if let date = date, let time = time {
+            let timeStamp = "Added on \(date) at \(time)"
+            cell.detailTextLabel?.text = timeStamp
+
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(items[indexPath.row].name!)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            // delete item at indexPath
+            
+            let item = self.items[indexPath.row]
+            self.context.delete(item)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            self.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        
+        let share = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+            // delete item at indexPath
+            
+            print("Share")
+            
+        }
+        
+        delete.backgroundColor = UIColor(red: 0/255, green: 177/255, blue: 106/255, alpha: 1.0)
+        share.backgroundColor = UIColor(red: 54/255, green: 215/255, blue: 183/255, alpha: 1.0)
+
+        
+        return [delete,share]
+        
+    }
+    
+}
---
+++ basicCoreDataExample-master/CoreDataExample/addItemViewController.swift
@@ -0,0 +1,111 @@
+//
+//  addItemViewController.swift
+//  CoreDataExample
+//
+//  Created by Farhan Syed on 4/16/17.
+//  Copyright © 2017 Farhan Syed. All rights reserved.
+//
+
+import UIKit
+
+class addItemViewController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var itemEntryTextView: UITextView?
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var chooseImageButton: UIButton!
+    @IBOutlet weak var imageViewPic: UIImageView!
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveContact(_ sender: Any) {
+        
+        guard let enteredText = itemEntryTextView?.text else {
+            return
+        }
+        
+        if enteredText.isEmpty || itemEntryTextView?.text == "Type anything..."{
+            print("No Data")
+            
+            let alert = UIAlertController(title: "Please Type Something", message: "Your entry was left blank.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default) { action in
+                
+            })
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM/dd/YY"
+            let currentDate = formatter.string(from: date)
+            
+            let timeFormatter = DateFormatter()
+            timeFormatter.timeStyle = .short
+            let currentTime = timeFormatter.string(from: date)
+            
+            guard let entryText = itemEntryTextView?.text else {
+                return
+            }
+            
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let newEntry = Item(context: context)
+            newEntry.name = entryText
+            newEntry.date = currentDate
+            newEntry.time = currentTime
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        itemEntryTextView?.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.black
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    @IBAction func btnClicked(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageViewPic.contentMode = .scaleAspectFit
+        imageViewPic.image = chosenImage
+        dismiss(animated:true, completion: nil)
+    }
+}
+
+
